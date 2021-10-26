294d324345f87699f959b2e1b3d80050335753c9
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2021-10-26 18:39:05 +0330

    add change file to menu

--- src/Main.java
+++ src/Main.java
@@ -3,77 +3,93 @@ import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class Main {
+
+    static File[] get_csv_files() {
+        File f = new File(".").getAbsoluteFile();
+        FilenameFilter textFilter = (dir, name) -> name.endsWith(".csv");
+        return f.listFiles(textFilter);
+    }
+
+    static int selectFile(File[] files) {
+        int i = 0;
+        for (File file : files)
+            System.out.printf("%d) %s\n", i++, file.getName());
+
+        System.out.print("select a file: ");
+        return new Scanner(System.in).nextInt();
+    }
+
+    static Matrix createMatrix(String filename) throws IOException {
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+
+            int height = Integer.parseInt(filename.split("\\(")[1].split(",")[0]);
+            int width = Integer.parseInt(filename.split("\\)")[0].split(",")[1]);
+            int[][] matrix = new int[height][];
+
+            String line;
+            for (int c = 0; (line = br.readLine()) != null; c++) {
+                matrix[c] = Stream.of(line.split(","))
+                        .mapToInt(Integer::parseInt).toArray();
+            }
+            return new Matrix(matrix, height, width);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 
-        File f = new File("J:\\Code\\Git\\DS\\phase2-AliSK81");
-
-        FilenameFilter textFilter = (dir, name) -> name.endsWith(".csv");
-        File[] files = f.listFiles(textFilter);
-        assert files != null;
-
-        for (int i = 0; i < files.length; i++) {
-            System.out.println(i + ") " + files[i].getName());
-        }
-
-        System.out.print("Choose a file: ");
-
-        String filename = files[sc.nextInt()].getName();
-        int height = Integer.parseInt(filename.split("\\(")[1].split(",")[0]);
-        int width = Integer.parseInt(filename.split("\\)")[0].split(",")[1]);
-
-        int[][] matrix = new int[height][];
-
-        FileReader fr = new FileReader(filename);
-        BufferedReader br = new BufferedReader(fr);
-        String line;
-
-        for (int c = 0; (line = br.readLine()) != null; c++) {
-            matrix[c] = Stream.of(line.split(","))
-                    .mapToInt(Integer::parseInt).toArray();
-        }
-
-        Matrix M = new Matrix(matrix, height, width);
-        System.out.println("matrix created.");
-
         while (true) {
+            File[] files = get_csv_files();
 
-            System.out.println("MENU:");
-            System.out.println("0) Insert -row -col -value");
-            System.out.println("1) Delete -row -col");
-            System.out.println("2) Search -value");
-            System.out.println("3) Update -row -col -value");
-            System.out.println("4) Print -type [0-1]");
-            System.out.println("5) Save File");
-            System.out.println("6) Exit");
+            int select = selectFile(files);
+            String filename = files[select].getName();
 
-            try {
-                switch (sc.nextInt()) {
-                    case 0 -> {
-                        M.insert(sc.nextInt(), sc.nextInt(), sc.nextInt());
-                        System.out.println("new node inserted.");
+            Matrix M = createMatrix(filename);
+            System.out.println("matrix created.");
+
+            boolean menu = true;
+            while (menu) {
+
+                System.out.println("MENU:");
+                System.out.println("0) Insert -row -col -value");
+                System.out.println("1) Delete -row -col");
+                System.out.println("2) Search -value");
+                System.out.println("3) Update -row -col -value");
+                System.out.println("4) Print -type [0-1]");
+                System.out.println("5) Save File");
+                System.out.println("6) Change File ");
+                System.out.println("7) Exit");
+
+                try {
+                    switch (sc.nextInt()) {
+                        case 0 -> {
+                            M.insert(sc.nextInt(), sc.nextInt(), sc.nextInt());
+                            System.out.println("new node inserted.");
+                        }
+                        case 1 -> {
+                            M.delete(sc.nextInt(), sc.nextInt());
+                            System.out.println("node deleted.");
+                        }
+                        case 2 -> {
+                            System.out.println("search result:");
+                            M.search(sc.nextInt());
+                        }
+                        case 3 -> {
+                            M.update(sc.nextInt(), sc.nextInt(), sc.nextInt());
+                            System.out.println("node updated.");
+                        }
+                        case 4 -> M.print(sc.nextInt());
+                        case 5 -> {
+                            M.save_file();
+                            System.out.println("file saved.");
+                        }
+                        case 6 -> menu = false;
+                        case 7 -> System.exit(0);
                     }
-                    case 1 -> {
-                        M.delete(sc.nextInt(), sc.nextInt());
-                        System.out.println("node deleted.");
-                    }
-                    case 2 -> {
-                        System.out.println("search result:");
-                        M.search(sc.nextInt());
-                    }
-                    case 3 -> {
-                        M.update(sc.nextInt(), sc.nextInt(), sc.nextInt());
-                        System.out.println("node updated.");
-                    }
-                    case 4 -> M.print(sc.nextInt());
-                    case 5 -> {
-                        M.save_file();
-                        System.out.println("file saved.");
-                    }
-                    case 6 -> System.exit(0);
+                } catch (RuntimeException ex) {
+                    System.out.println(ex.getMessage());
                 }
-            } catch (RuntimeException ex) {
-                System.out.println(ex.getMessage());
             }
         }
     }
